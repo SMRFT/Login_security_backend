6cf0f8205bbb8fd613bd777dc7ed81c69d966b39
SMRFT <parthibansmrft@gmail.com>
2025-08-12 13:50:15 +0530

    adding the email validatation

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
 from . import views
+from .forgetpassword import forgot_password,verify_reset_token,reset_password
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('get_modules/', views.getmodules, name='getmodules'),
     path('get_data_entitlements', views.get_data_entitlements, name='get_data_entitlements'),
+    path('forgot-password/', forgot_password, name='forgot_password'),
+    path('verify-reset-token/', verify_reset_token, name='verify_reset_token'),
+    path('reset-password/', reset_password, name='reset_password'),
 ]
--- backend/settings.py
+++ backend/settings.py
@@ -147,3 +147,13 @@ CORS_ALLOW_HEADERS = [
 ]
 
 STATIC_URL = 'static/'
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'shanmugahospitalhr@gmail.com'
+EMAIL_HOST_PASSWORD = 'ykrs ylby ssro biwp'  
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
