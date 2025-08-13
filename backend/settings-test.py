aed9802df5e1fd20d70100669466df50ef1bc8a1
SMRFT <110091550+SMRFT@users.noreply.github.com>
2025-08-13 13:43:57 +0530

    Update settings-test.py

--- backend/settings-test.py
+++ backend/settings-test.py
@@ -135,3 +135,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True  # Allow all origins for development
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'shanmugahospitalhr@gmail.com'
+EMAIL_HOST_PASSWORD = 'ykrs ylby ssro biwp'  
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
